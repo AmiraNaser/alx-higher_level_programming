6c202b030123cce538d4eccf96fe8ee67670ee3c
AmiraNaser <meronaser78@gmail.com>
2024-06-20 23:14:01 +0300

    rest of tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+const args = process.argv;
+if (args.length <= 3) console.log(0);
+else {
+  console.log(args.map(Number).slice(2, args.length).sort((a, b) => b - a)[1]);
+}
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const args = parseInt(process.argv[2]);
+console.log(args);
+if (isNaN(args)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + args);
+}
