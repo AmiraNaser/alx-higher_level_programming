f32878f123d168ec7445697fc00b070e77eb1451
AmiraNaser <meronaser78@gmail.com>
2024-06-21 17:30:37 +0300

    advanced tasks

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const n in dict) {
+  if (newDict[dict[n]] === undefined) {
+    newDict[dict[n]] = [];
+  }
+  newDict[dict[n]].push(n);
+}
+console.log(newDict);
---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const fs = require('fs');
+const args = process.argv.slice(2);
+const fileA = fs.readFileSync('./' + args[0]);
+const fileB = fs.readFileSync('./' + args[1]);
+fs.writeFileSync('./' + args[2], fileA + fileB + '\n');
